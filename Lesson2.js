f81ab2963b5fe68bf393d068d7fc2c25164620ba
Nadezhda <nadya.durina@gmail.com>
2023-01-29 11:55:53 +0300

    add HW files

---
+++ Lesson2.js
@@ -0,0 +1,34 @@
+let v1 = 10
+let v2 = 10.5
+let v3 = true
+let v4 = false
+let v5 = null
+let v6 = [10, 11, 44]
+let v7 = {"name": "Alex", "age":25, "salary":2000}
+let v8 = "Hello"
+
+//console.log(v1, typeof v1)
+//console.log(v2, typeof v2)
+//console.log(v3, typeof v3)
+//console.log(v4, typeof v4)
+//console.log(v5, typeof v5)
+//console.log(v6, typeof v6)
+//console.log(v7, typeof v7)
+//console.log(v8, typeof v8)
+
+let item1 = 10
+let item2 = 20
+
+let result = item1 > item2
+
+//console.log("result =", result)
+
+if(result){
+    console.log("result  =", result)
+} else {
+    
+    console.log("other result")
+}
+
+
+
---
+++ Second HW.js
@@ -0,0 +1,27 @@
+//Сделать функцию которая при вызове напишет в консоль сумму 2-х переданных в неё чисел
+function Summa(S1, S2) {
+    console.log(S1 + S2);
+   };
+   
+   Summa(10, 21);
+
+
+//Сделать функцию которая вернёт в консоль квадрат переданного числа
+function Square(Q1) {
+    return Q1*Q1;
+   };
+   console.log(Square(3));
+   
+ 
+//Сделать функцию. На вход принимaет 3 параметра (Ф,И,О). Вернёт JSON
+function User(name, surname, middlname) {
+    
+    console.log(name + " " + surname + " " + middlname); 
+    
+   };
+   
+ User ('Anna', 'Smith', 'Tomovna');
+
+ // 
+ function F1 ()
+ 
